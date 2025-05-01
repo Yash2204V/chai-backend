12ad4fca2a8ba283b94a5333d9f13ec054742f2e
YASH2204V <yash.v220104@gmail.com>
2025-05-01 17:58:36 +0530

    Refactor comment and video controllers; add comment creation and update logic

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,110 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
 
+    
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    /* 
+        1. take the content from the user, req.body.
+        2. Check the existence of it.
+        3. Then, get the video id from the params.
+        4. get the user id from the req.user (token)
+        5. Then, store & save them in the comments db.
+    */
+    const { content } = req.body;
+    const videoId = req.params.videoId;
+    const userId = req.user._id;
+    
+    if (!content) {
+        throw new ApiError(400, "Content is invalid");
+    }
+    if (!videoId) {
+        throw new ApiError(400, "Video Id doesn't exist")
+    }
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: userId
+    });
+    await comment.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, comment, "Comment created successfully")
+        )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    /* 
+        1. Get the commentId from the params.
+        2. get and update the comment schema.
+        3. then, send the response.
+    */
+
+    const { commentId } = req.params;
+    const { updatedContent } = req.body;
+
+    if (!updatedContent) {
+        throw new ApiError(400, "Content is invalid")
+    }
+
+    if (!commentId) {
+        throw new ApiError(400, "Comment Id is invalid.");
+    }
+
+    const updateComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set: {
+                content: updatedContent,
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updateComment, "Comment updated successfully")
+        )
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+
+    const { commentId } = req.params;
+    if (!commentId) {
+        throw new ApiError(400, "Comment Id is invalid.")
+    }
+
+    const comment = await Comment.findByIdAndDelete(commentId);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, comment, "Comment deleted successfully.")
+    )
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,11 +1,10 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose"
+import { Video } from "../models/video.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
@@ -13,13 +12,63 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    const { title, description } = req.body;
     // TODO: get video, upload to cloudinary, create video
+
+    /* 
+        1. get the thumbnail and videofile name from multer.
+        2. then, take the filePath and send it to cloudinary.
+        3. extract the duration from the cloudinary output. 
+        4. take the userId as owner.
+        5. ignore for now: duration, views, isPublished.
+        6. extract the duration from the cloudinary output.
+        7. save all videoFile, thumbnail, owner, title, description.
+    */
+
+    if (!(title || description)) {
+        throw new ApiError(400, "Title or Description is invalid")
+    }
+
+    const videoLocalPath = req.files?.videoFile[0]?.path;
+    if (!videoLocalPath){
+        throw new ApiError(400, "Video path is required")
+    }
+
+    let thumbnailLocalPath;
+    if(req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0 ) {
+        thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+    }
+
+    const videoFile = await uploadOnCloudinary(videoLocalPath);
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    if (!(videoFile || thumbnail)) {
+        throw new ApiError("Error while uploading file on cloudinary")
+    }
+
+
+    const video = await Video.create({
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        owner: req.user._id,
+        title,
+        description,
+        duration: videoFile.duration
+    })
+    await video.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, video, "Video published successfully")
+        )
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
